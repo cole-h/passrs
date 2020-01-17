f7bf2b4907a1a2df2ea8daf8f34871265a5d77f7
Cole Helbling <cole.e.helbling@outlook.com>
2020-01-16 20:50:10 -0800

    Update CI to decrypt test secrets
    Also adds tests for decrypting secrets

--- tests/util.rs
+++ tests/util.rs
@@ -45,9 +45,42 @@ fn check_sneaky_paths() {
     assert!(util::check_sneaky_paths("..").is_err());
     assert!(util::check_sneaky_paths("/../password").is_err());
     assert!(util::check_sneaky_paths("amazon/../password").is_err());
+    assert!(util::check_sneaky_paths("password").is_ok());
 }
 
 #[test]
 fn find_target_single() {
     assert!(util::find_target_single(".").unwrap().len() > 0);
+    assert!(util::find_target_single("a").unwrap().len() == 1);
+    assert!(util::find_target_single("z").is_err());
+}
+
+#[test]
+fn decrypt_file_into_bytes() {
+    let file = util::canonicalize_path("a").unwrap();
+    let contents = util::decrypt_file_into_bytes(&file).unwrap();
+
+    assert_eq!(contents, "eHy;CDpa&4]Sf1g*rx1Zlrig".as_bytes());
+}
+
+#[test]
+fn decrypt_file_into_strings() {
+    let file = util::canonicalize_path("f").unwrap();
+    let contents = util::decrypt_file_into_strings(&file).unwrap();
+    let mut iter = contents.iter();
+
+    assert!(contents.len() == 6);
+    assert_eq!(iter.next(), Some(&String::from("a")));
+    assert_eq!(iter.next(), Some(&String::from("b")));
+    assert_eq!(iter.next(), Some(&String::from("c")));
+    assert_eq!(iter.next(), Some(&String::from("d")));
+    assert_eq!(iter.next(), Some(&String::from("e")));
+    assert_eq!(iter.next(), Some(&String::from("f")));
+    assert_eq!(iter.next(), None);
+}
+
+#[test]
+fn find_gpg_id() {
+    assert!(util::find_gpg_id("/").is_err());
+    assert!(util::find_gpg_id(&*consts::PASSWORD_STORE_DIR).is_ok());
 }
