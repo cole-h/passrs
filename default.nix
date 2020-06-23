d18632517961daba4853777324200aa18beeb769
Cole Helbling <cole.e.helbling@outlook.com>
2020-06-22 23:31:33 -0700

    update ci and default.nix
    * ci: don't add a channel, since we use a pinned nixpkgs thanks to niv
    
    Also update to v10 because why not.
    
    * default.nix: make src usable for both git checkouts and tarballs
    
    * default.nix: don't add completions to the store
    
    By not referring to the fish completion by name, passrs.fish won't
    appear in the store except as part of passrs's package.

--- default.nix
+++ default.nix
@@ -12,9 +12,19 @@ naersk.buildPackage {
   pname = "passrs";
   version = lib.commitIdFromGitRepo ./.git;
 
-  src = builtins.filterSource
-    (path: type: type != "directory" || builtins.baseNameOf path != "target")
-    (toString ./.);
+  src =
+    let
+      dir = src: builtins.readDir src;
+      isGit = src: (dir src) ? ".git" && (dir src).".git" == "directory";
+      getSrc = src:
+        if isGit src then
+          fetchGit src
+        else
+          builtins.filterSource
+            (path: type: type != "directory" || builtins.baseNameOf path != "target")
+            src;
+    in
+    getSrc ./.;
 
   nativeBuildInputs = with pkgs; [
     git # for build script to retrieve git hash and add to version info
@@ -36,7 +46,11 @@ naersk.buildPackage {
 
   # NOTE: Completions require the gpg2 binary to be in path in order to complete
   # keys for commands like `passrs init`
+  #
+  # `|| true` is necessary because postInstall is run for both the
+  # dependencies and the actual package.
+  # https://github.com/nmattia/naersk/issues/105
   postInstall = ''
-    installShellCompletion --fish ${./completions/passrs.fish}
+    installShellCompletion --fish completions/passrs.fish || true
   '';
 }
