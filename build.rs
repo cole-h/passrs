07a0729708313c1661e208757b215016eb7d340c
Cole Helbling <cole.e.helbling@outlook.com>
2020-08-30 12:16:22 -0700

    build: support both env and cmd revisions
    People who don't use Nix wouldn't have had a git revision in the
    version. By reviving the `git` command to retrieve the revision, now
    they will.

--- build.rs
+++ build.rs
@@ -1,11 +1,15 @@
-use std::env;
-
 fn main() {
     let ver = env!("CARGO_PKG_VERSION");
-    let rev = env::var("PASSRS_REV");
+    let rev_env = std::env::var("PASSRS_REV");
+    let rev_cmd = std::process::Command::new("git")
+        .args(&["rev-parse", "--short", "HEAD"])
+        .output()
+        .ok()
+        .and_then(|r| String::from_utf8(r.stdout).ok());
 
-    let version = match rev {
-        Ok(rev) => format!("{} ({})", ver, rev.trim()),
+    let version = match (rev_env, rev_cmd) {
+        (Ok(rev), _) => format!("{} ({})", ver, rev.trim()),
+        (_, Some(rev)) => format!("{} ({})", ver, rev.trim()),
         _ => format!("{} (unknown)", ver),
     };
 
